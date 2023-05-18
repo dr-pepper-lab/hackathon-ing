04b3d4d3a26484202976c891e10b080c67fa1417
StachowiakDawid <dawidos234@gmail.com>
2023-05-18 21:22:39 +0200

    Test whole folders

--- prepare_model.py
+++ prepare_model.py
@@ -111,19 +111,3 @@ epochs_range = range(epochs)
 
 model.save('IngModel.h5')
 model.save_weights('IngModelWeights')
-
-pred_path = sys.argv[1]
-
-img = tf.keras.utils.load_img(
-    pred_path, target_size=(img_height, img_width)
-)
-img_array = tf.keras.utils.img_to_array(img)
-img_array = tf.expand_dims(img_array, 0)
-
-predictions = model.predict(img_array)
-score = tf.nn.softmax(predictions[0])
-
-print(
-    "This image most likely belongs to {} with a {:.2f} percent confidence."
-    .format(class_names[np.argmax(score)], 100 * np.max(score))
-)
--- test_image.py
+++ test_image.py
@@ -2,6 +2,7 @@ import numpy as np
 import tensorflow as tf
 import sys
 import cv2
+import os
 
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
@@ -10,29 +11,34 @@ class_names = ["advertisement", "budget", "email", "file_folder", "form", "handw
 
 img_height = 200
 img_width = 160
+model = tf.keras.models.load_model('IngModel.h5')
 
 pred_path = sys.argv[1]
 
-img = tf.keras.utils.load_img(
-    pred_path, target_size=(img_height, img_width)
-)
-img_array = tf.keras.utils.img_to_array(img)
-img_array = tf.expand_dims(img_array, 0)
+def test_image(path):
+    img = tf.keras.utils.load_img(
+        path, target_size=(img_height, img_width)
+    )
+    img_array = tf.keras.utils.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)
 
+    predictions = model.predict(img_array)
+    score = tf.nn.softmax(predictions[0])
 
+    print(
+        "This image most likely belongs to {} with a {:.2f} percent confidence."
+        .format(class_names[np.argmax(score)], 100 * np.max(score))
+    )
 
-model = tf.keras.models.load_model('IngModel.h5')
-predictions = model.predict(img_array)
-score = tf.nn.softmax(predictions[0])
+    img = cv2.imread(path,cv2.IMREAD_COLOR)
+    img = cv2.putText(img, class_names[np.argmax(score)], (50, 180), cv2.QT_FONT_NORMAL, 
+                    4, (194,24,91), 10, cv2.LINE_AA)
+    cv2.imshow('Tested image', img)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
 
-print(
-    "This image most likely belongs to {} with a {:.2f} percent confidence."
-    .format(class_names[np.argmax(score)], 100 * np.max(score))
-)
-
-img = cv2.imread(sys.argv[1] ,cv2.IMREAD_COLOR)
-img = cv2.putText(img, class_names[np.argmax(score)], (50, 180), cv2.QT_FONT_NORMAL, 
-                   4, (194,24,91), 10, cv2.LINE_AA)
-cv2.imshow('Tested image', img)
-cv2.waitKey()
-cv2.destroyAllWindows()
+if os.path.isfile(pred_path):
+    test_image(pred_path)
+elif os.path.isdir(pred_path):
+    for img in os.scandir(pred_path):
+        test_image(img.path)
