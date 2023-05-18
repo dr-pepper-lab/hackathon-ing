fab7f10be5f4f41cd66fc7c4219de86e18af19b9
StachowiakDawid <dawidos234@gmail.com>
2023-05-18 18:08:50 +0200

    Try loading model in test_image.py

--- test_image.py
+++ test_image.py
@@ -1,15 +1,12 @@
-import matplotlib
-import PIL
-import pathlib
-import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 import sys
 import cv2
 
-from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
+
+class_names = ["advertisement", "budget", "email", "file_folder", "form", "handwritten", "invoice", "letter", "memo", "news_article", "pit37_v1", "pozwolenie_uzytkowanie_obiektu_budowlanego", "presentation", "questionnaire", "resume", "scientific_publication", "scientific_report", "specification", "umowa_na_odleglosc_odstapienie", "umowa_o_dzielo", "umowa_sprzedazy_samochodu"]
 
 img_height = 200
 img_width = 160
@@ -22,6 +19,23 @@ img = tf.keras.utils.load_img(
 img_array = tf.keras.utils.img_to_array(img)
 img_array = tf.expand_dims(img_array, 0)
 
+model = Sequential([
+  layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+  layers.Conv2D(16, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(32, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(64, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Flatten(),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(len(class_names))
+])
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.load_weights('checkpoint')
 predictions = model.predict(img_array)
 score = tf.nn.softmax(predictions[0])
 
