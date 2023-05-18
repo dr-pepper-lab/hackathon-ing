c390d3e2de81c3994397a349a40fe4a7653855c5
StachowiakDawid <dawidos234@gmail.com>
2023-05-18 18:21:18 +0200

    remove redundant imports

--- prepare_model.py
+++ prepare_model.py
@@ -1,7 +1,5 @@
-import matplotlib
-import PIL
-import pathlib
 import matplotlib.pyplot as plt
+import pathlib
 import numpy as np
 import tensorflow as tf
 import sys
@@ -12,7 +10,7 @@ from tensorflow.keras.models import Sequential
 
 data_dir = pathlib.Path("prepared_source_images")
 
-image_count = len(list(data_dir.glob('*/*.tiff')))+len(list(data_dir.glob('*/*.jpg')))
+image_count = len(list(data_dir.glob('*/*.jpg')))
 print(image_count)
 
 labels = {"advertisement": 0, "budget": 1, "email": 2, "file_folder": 3, "form": 4, "handwritten": 5, "invoice": 6, "letter": 7, "memo": 8, "news_article": 9, "pit37_v1": 10, "pozwolenie_uzytkowanie_obiektu_budowlanego": 11, "presentation": 12, "questionnaire": 13, "resume": 14, "scientific_publication": 15, "scientific_report": 16, "specification": 17, "umowa_na_odleglosc_odstapienie": 18, "umowa_o_dzielo": 19, "umowa_sprzedazy_samochodu": 20}
@@ -48,8 +46,6 @@ class_names = train_ds.class_names
 print(class_names)
 
 # visualize
-
-import matplotlib.pyplot as plt
 
 plt.figure(figsize=(10, 10))
 for images, labels in train_ds.take(1):
