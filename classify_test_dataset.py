0bcc4c587f8176550f8833e1f86ae5d81564e0ef
StachowiakDawid <dawidos234@gmail.com>
2023-05-19 01:05:09 +0200

    Init script to classify data

--- classify_test_dataset.py
+++ classify_test_dataset.py
@@ -1,9 +1,7 @@
 import numpy as np
 import tensorflow as tf
 import sys
-import cv2
 import os
-
 
 class_names = ["advertisement", "budget", "email", "file_folder", "form", "handwritten", "invoice", "letter", "memo", "news_article", "pit37_v1", "pozwolenie_uzytkowanie_obiektu_budowlanego", "presentation", "questionnaire", "resume", "scientific_publication", "scientific_report", "specification", "umowa_na_odleglosc_odstapienie", "umowa_o_dzielo", "umowa_sprzedazy_samochodu"]
 
@@ -22,20 +20,8 @@ def test_image(path):
 
     predictions = model.predict(img_array)
     score = tf.nn.softmax(predictions[0])
+    img_class = class_names[np.argmax(score)]
 
-    print(
-        "This image most likely belongs to {} with a {:.2f} percent confidence."
-        .format(class_names[np.argmax(score)], 100 * np.max(score))
-    )
-
-    img = cv2.imread(path,cv2.IMREAD_COLOR)
-    title = str(100*np.max(score))[0:2] + "% " +  class_names[np.argmax(score)]
-    cv2.imshow(title, img)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
-
-if os.path.isfile(pred_path):
-    test_image(pred_path)
-elif os.path.isdir(pred_path):
+if os.path.isdir(pred_path):
     for img in os.scandir(pred_path):
         test_image(img.path)
--- prepare_model.py
+++ prepare_model.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 import pathlib
 import numpy as np
 import tensorflow as tf
-import sys
 
 from tensorflow import keras
 from tensorflow.keras import layers
