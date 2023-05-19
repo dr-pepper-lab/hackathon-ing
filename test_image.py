10d4d2dab34583ccf1e190e92c259a5de48c5bd1
StachowiakDawid <dawidos234@gmail.com>
2023-05-19 10:29:15 +0200

    Verbose=none

--- test_image.py
+++ test_image.py
@@ -18,7 +18,7 @@ def test_image(path, print_answer=False):
     img_array = tf.keras.utils.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0)
 
-    predictions = model.predict(img_array)
+    predictions = model.predict(img_array,verbose=None)
     score = tf.nn.softmax(predictions[0])
 
     class_names = ["advertisement", "budget", "email", "file_folder", "form", "handwritten", "invoice", "letter", "memo", "news_article", "pit37_v1", "pozwolenie_uzytkowanie_obiektu_budowlanego", "presentation", "questionnaire", "resume", "scientific_publication", "scientific_report", "specification", "umowa_na_odleglosc_odstapienie", "umowa_o_dzielo", "umowa_sprzedazy_samochodu"]
